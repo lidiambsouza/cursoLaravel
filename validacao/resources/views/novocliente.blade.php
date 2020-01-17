e156d209e82bf08e2503d1aa40370b22a263d958
lidia <lidiambsouza@gmail.com>
2020-01-17 17:57:21 -0300

    até aula 82

---
+++ validacao/resources/views/novocliente.blade.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <title>Cadastro de Cliente</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <meta name="csrf-token" content="{{csrf_token()}}">
+        <style>
+            body{
+                padding: 20px;
+            }
+        </style>
+    </head>
+    <body>
+        <main role="main">
+            <div class="row">
+                <div class="container col-sm-8 offset-md-2">
+                    <div class="card border">
+                        <div class="card-header">
+                            <div class="card-title">
+                                Cadastro de Cliente
+                            </div>
+                        </div>
+                        <div class="card-body">
+                            <form action="/cliente" method="post">
+                                <div class="form-group">
+                                    <label for="nome">Nome do cliente</label>
+                                    <input type="text" id="nome" class="form-control" name="nome" placeholder="Nome do Cliente">
+                                </div>
+                                <div class="form-group">
+                                    <label for="idade">Idade do cliente</label>
+                                    <input type="number" id="idade" class="form-control" name="idade" placeholder="Idade do Cliente">
+                                </div>
+                                <div class="form-group">
+                                    <label for="endereco">Endereço do cliente</label>
+                                    <input type="text" id="endereco" class="form-control" name="endereco" placeholder="Endereço do Cliente">
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">E-mail do cliente</label>
+                                    <input type="text" id="email" class="form-control" name="email" placeholder="E-mail do Cliente">
+                                </div>
+                                <button type="submit" class="btn btn-sm btn-primary">Salvar</button>
+                                <button type="cancel" class="btn btn-sm btn-danger">Cancelar</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <script src="{{asset('js/app.js')}}" type="text/javascript"></script>
+    </body>
+</html>
